4f0017fa62517b599da263b476f4bebd8777d920
Amar <108946547+AmarVcode@users.noreply.github.com>
2023-04-20 01:56:32 +0530

    Update contact.php

--- php/contact.php
+++ php/contact.php
@@ -1,12 +1,13 @@
 <?php
 $name = $_POST["name"];
   $email = $_POST["email"];
+  $phone = $_POST["phoneNumber"];
   $bussiness = $_POST["bussiness"];
   $message = $_POST["message"];
 
   $to = "info@robowale.com";
   $subject = "new Message form Robowale";
-  $body = "Name: $name\nEmail: $email\nMessage: $message";
+  $body = "Name: $name\nEmail: $email\nPhone Number: $phone\nMessage: $message";
 
   if (mail($to, $subject, $body)) {
     echo "Thank you for contacting us!";
